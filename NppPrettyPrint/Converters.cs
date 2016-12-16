f252f3d3847238ad4acb3f821acb3db9f08d55a3
Erik Pilsits <epilsits@gmail.com>
2016-12-16 12:25:40 -0600

    Refactoring Converters

---
+++ NppPrettyPrint/Converters.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Converters
+{
+    public class Base64GzipConverter
+    {
+        public static string ConvertToString(StringBuilder sIn)
+        {
+            byte[] bIn = Convert.FromBase64String(sIn.ToString());
+            using (var msIn = new MemoryStream(bIn))
+            using (var gz = new GZipStream(msIn, CompressionMode.Decompress))
+            using (var sr = new StreamReader(gz))
+                return sr.ReadToEnd();
+        }
+
+        public static string ConvertToPayload(StringBuilder sIn)
+        {
+            using (var msOut = new MemoryStream())
+            {
+                using (var msIn = new MemoryStream(Encoding.UTF8.GetBytes(sIn.ToString())))
+                using (var gz = new GZipStream(msOut, CompressionMode.Compress, true))
+                    msIn.CopyTo(gz);
+
+                return Convert.ToBase64String(msOut.ToArray(), Base64FormattingOptions.None);
+            }
+
+        }
+    }
+}
--- NppPrettyPrint/Formatters.cs
+++ NppPrettyPrint/Formatters.cs
@@ -1,8 +1,6 @@
-﻿using ijsonDotNet;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -134,34 +132,6 @@ namespace Formatters
         public static void ValidateXml(StringBuilder sIn)
         {
             var element = XDocument.Parse(sIn.ToString());
-        }
-    }
-}
-
-namespace Converters
-{
-    public class Base64GzipConverter
-    {
-        public static string ConvertToString(StringBuilder sIn)
-        {
-            byte[] bIn = Convert.FromBase64String(sIn.ToString());
-            using (var msIn = new MemoryStream(bIn))
-            using (var gz = new GZipStream(msIn, CompressionMode.Decompress))
-            using (var sr = new StreamReader(gz))
-                return sr.ReadToEnd();
-        }
-
-        public static string ConvertToPayload(StringBuilder sIn)
-        {
-            using (var msOut = new MemoryStream())
-            {
-                using (var msIn = new MemoryStream(Encoding.UTF8.GetBytes(sIn.ToString())))
-                using (var gz = new GZipStream(msOut, CompressionMode.Compress, true))
-                    msIn.CopyTo(gz);
-
-                return Convert.ToBase64String(msOut.ToArray(), Base64FormattingOptions.None);
-            }
-
         }
     }
 }
